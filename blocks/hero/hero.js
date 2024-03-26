73115014376aa6426a18b23fb65d5a98db9b1c3f
poojethaa-kumar-bounteous <164335379+poojethaa-kumar-bounteous@users.noreply.github.com>
2024-03-26 17:31:32 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,9 +1,15 @@
 export default async function decorate(block) {
+  
   const hero = document.createElement('div');
+  
   const p = document.createElement('p');
-  p.innerHTML = "Some text that should be appended...";
+  
+  p.innerHTML = 'Some text that should be appended...';
+  
   hero.appendChild(p);
+  
   hero.innerHTML = `<button type="button">Click here
-    </button>`
+    </button>`;
+  
   block.append(hero)
 }
