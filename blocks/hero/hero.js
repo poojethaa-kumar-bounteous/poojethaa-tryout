3328d27a0b2d973cbd9d39eef641859689e9b93b
poojethaa kumar <164335379+poojethaa-kumar-bounteous@users.noreply.github.com>
2024-03-28 12:46:53 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,7 +3,7 @@ export default async function decorate(block) {
   const p = document.createElement('p');
   p.textContent = 'Description text for paragraph content';
   hero.appendChild(p);
-  hero.innerHTML = `<button type="button">Click here
+  hero.innerHTML = `<button type="button" class="button">Click here
     </button>`;
   block.append(hero);
 }
