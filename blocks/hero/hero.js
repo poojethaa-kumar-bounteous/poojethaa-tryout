1b3e04ddc3a9c95bdf829a30d8e0813ead4be60f
poojethaa kumar <164335379+poojethaa-kumar-bounteous@users.noreply.github.com>
2024-03-28 17:54:12 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,21 +1,9 @@
-// /* adding the extra content in hero component */
-// export default async function decorate(block) {
-//   const hero = document.createElement('div');
-//   const p = document.createElement('p');
-//   p.textContent = 'Description text for paragraph content';
-//   hero.append(p);
-//   hero.innerHTML = `<button type="button" class="button">Click here
-//     </button>`;
-//   block.append(hero);
-// }
-
-/**
- * decorates the header, mainly the nav
- * @param {Element} block The header block element
- */
 export default async function decorate(block) {
-  // load nav as fragment
-  const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
-  const fragment = await loadFragment(navPath);
+  const hero = document.createElement('div');
+  const p = document.createElement('p');
+  p.textContent = 'Description text for paragraph content';
+  hero.append(p);
+  hero.innerHTML = `<button type="button" class="button">Click here
+    </button>`;
+  block.append(hero);
 }
